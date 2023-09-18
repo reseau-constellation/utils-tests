7c958530f7b282534e4527fc5217a667aeb3b93b
julienmalard <julien.malard@mail.mcgill.ca>
2023-09-18 17:32:04 +0200

    Régler fichier aegir

--- .aegir.js
+++ .aegir.js
@@ -1,9 +1,6 @@
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { createRequire } from "module";
-import express from "express";
-import cors from "cors";
-import url from "url";
 
 const require = createRequire(import.meta.url);
 
@@ -45,67 +42,6 @@ const options = {
       config: {
         buildConfig: esbuild,
       },
-    },
-    before: async (opts) => {
-      // On va lancer une page Constellation pour pouvoir tester la connectivité webrtc avec les navigateurs
-      const { chromium } = await import("playwright");
-      const navigateur = await chromium.launch();
-      try {
-        const page = await navigateur.newPage();
-        await page.goto("https://réseau-constellation.ca");
-      } catch (e) {
-        // On arrête pas les tests pour une petite erreur comme ça
-        console.error(e);
-      }
-
-      // Pour pouvoir accéder les fichiers test dans le navigateur
-      /**
-       * @type {Express | undefined}
-       */
-      let serveurLocal = undefined;
-
-      if (
-        opts.target.includes("browser") ||
-        opts.target.includes("electron-renderer")
-      ) {
-        const appliExpress = express();
-
-        // Permettre l'accès à partir de l'hôte locale
-        appliExpress.use(
-          cors({
-            origin: function (origin, callback) {
-              if (!origin) {
-                callback(null, true);
-                return;
-              }
-              if (new URL(origin).hostname === "127.0.0.1") {
-                callback(null, true);
-              } else {
-                callback(new Error("Not allowed by CORS"));
-              }
-            },
-          })
-        );
-        appliExpress.get("/fichier/:nomFichier", function (req, res) {
-          const { nomFichier } = req.params;
-
-          const cheminFichier = path.join(
-            url.fileURLToPath(new URL(".", import.meta.url)),
-            "test",
-            "ressources",
-            decodeURIComponent(nomFichier)
-          );
-
-          res.sendFile(cheminFichier);
-        });
-        serveurLocal = appliExpress.listen(3000);
-      }
-
-      return { navigateur, serveurLocal };
-    },
-    after: async (_, avant) => {
-      await avant.navigateur.close();
-      await avant.serveurLocal?.close();
     },
   },
   build: {
