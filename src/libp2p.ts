c3a555ac119424e80c3ba8300b9bbbc00267a696
julienmalard <julien.malard@mail.mcgill.ca>
2025-02-03 13:24:57 -0500

    version

--- src/libp2p.ts
+++ src/libp2p.ts
@@ -34,7 +34,7 @@ import { gossipsub } from "@chainsafe/libp2p-gossipsub";
 import { circuitRelayTransport } from "@libp2p/circuit-relay-v2";
 import type { PrivateKey } from "@libp2p/interface";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type {ProgressEvent} from "progress-events";
+import type { ProgressEvent } from "progress-events";
 
 /**
  * A basic Libp2p configuration for Node.js nodes.
