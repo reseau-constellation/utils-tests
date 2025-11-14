1dc2a0de063b4481e77874099fe3072f125c86ba
julienmalard <julien.malard@mail.mcgill.ca>
2025-11-14 11:07:32 +0530

    Rajouter le préfixe

--- src/dossiers.ts
+++ src/dossiers.ts
@@ -7,10 +7,11 @@ export const dossierTempo = async (): Promise<{
 }> => {
   if (isNode || isElectronMain) {
     const fs = await import("fs");
+    const path = await import("path");
     const os = await import("os");
     const sync = (await import("rimraf")).sync;
 
-    const dossier = fs.mkdtempSync(os.tmpdir());
+    const dossier = fs.mkdtempSync(path.join(os.tmpdir(), 'constl'));
     const effacer = () => {
       try {
         sync(dossier);
